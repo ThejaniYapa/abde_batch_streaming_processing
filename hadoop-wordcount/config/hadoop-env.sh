f95e6870994351a723378e1632051a9f4fc26397
ThejaniYapa <thejyapa95@gmail.com>
2025-12-21 14:10:38 +0000

    Docker compose finalized

--- hadoop-wordcount/config/hadoop-env.sh
+++ hadoop-wordcount/config/hadoop-env.sh
@@ -1,1 +1,5 @@
-export JAVA_HOME=/usr/local/openjdk
+# export JAVA_HOME=/usr/local/openjdk
+# export JAVA_HOME=${JAVA_HOME:-/opt/java/openjdk}
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+export HDFS_NAMENODE_DIR=/hadoopdata/hdfs/namenode
+export HDFS_DATANODE_DIR=/hadoopdata/hdfs/datanode
